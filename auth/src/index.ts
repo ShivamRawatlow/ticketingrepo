93fca9cb9745f819f8451ad0835c0891f5f74cba
shivam <thelegendofawizard@gamil.com>
2020-07-07 20:48:53 +0530

    Unhandeled promise rejection error

---
+++ auth/src/index.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+import mongoose from 'mongoose';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.get('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT Key must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!!!');
+});
+
+start();
